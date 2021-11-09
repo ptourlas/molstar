14a3502a6ad0a9de772678ebda36faa1346c302a
ptourlas <p.tourlas@acellera.com>
2021-11-09 15:25:44 +0200

    (lint fiix)

--- src/mol-io/reader/jsarray/parser.ts
+++ src/mol-io/reader/jsarray/parser.ts
@@ -4,7 +4,7 @@ import { Task } from '../../../mol-task';
 export function parseArray(data: number[][]) {
     return Task.create<Result<number[][]>>('Parse ', async ctx => {
         try {
-            /*this is where we supposedly check for structural errors?*/
+            /* this is where we supposedly check for structural errors? */
             console.log('PARSING:', data)
             return Result.success(data);
         } catch (e) {
